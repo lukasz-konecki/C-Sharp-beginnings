839f83f7c80ecc678ebb8f145a2ba596ee2357cb
lukasz-konecki <42964346+lukasz-konecki@users.noreply.github.com>
2018-09-05 21:06:28 +0200

    Add files via upload

---
+++ Averager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Averager
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the program that calculates an average of all the provided numbers. Please provide the first number. To exit write \"quit\".");
+            var newNumber = Console.ReadLine();
+            var i = 0;
+            var runningTotal = 0.0;
+            while (newNumber.ToLower()!="quit")
+            {
+                if (double.TryParse(newNumber, out double ToBeAdded))
+                {
+                    runningTotal += ToBeAdded;
+                    i++;
+                }
+                else
+                Console.WriteLine("You provided something else than a number. Please try again or type \"quit\" to exit.");
+                newNumber = Console.ReadLine();
+            }
+            if(i==0)
+            Console.WriteLine("There is no average because you didn't provide any number. Bye.");
+            else
+            Console.WriteLine("The average is " + (runningTotal/i) + ".");
+        }
+    }
+}
